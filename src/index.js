96d8b9835b148b0d68a47a0060a7b75c209da661
Ednilton Junior <jrjunior_cg@live.com>
2020-07-29 18:14:39 -0300

    Att

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ const puppeter = require('puppeteer');
 const cron = require("node-cron");
 const { promisify } = require('util')
 
-cron.schedule("*/3 * * * *", async ()=> {
+cron.schedule("*/1 * * * *", async ()=> {
 
     const sleep = promisify(setTimeout);
 
     let browser = await puppeter.launch({
-        headless: true, 
         executablePath: "/usr/bin/google-chrome-stable",
         userDataDir: "/home/junior/.config/google-chrome/Default"
     });
@@ -19,9 +18,10 @@ cron.schedule("*/3 * * * *", async ()=> {
         await page.goto('https://app.powerbi.com/groups/3a6265fd-42bd-481d-8e81-ad593f226c00/reports/e446003a-b3bf-42c3-a682-b95664d780a7/ReportSectionf2f1a5a4b9c2d45c063b');
     
     
-    
-        await page.waitFor('a[data-event-property="signin"]');
-        await page.click('a[data-event-property="signin"]');
+        // await browser.close();
+
+        // await page.waitFor('a[data-event-property="signin"]');
+        // await page.click('a[data-event-property="signin"]');
     
         
         await page.waitFor('button[ng-click="$ctrl.showRelatedContentPane()"]');
